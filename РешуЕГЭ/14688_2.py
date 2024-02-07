6d1ffcbc49966aa2513bbc81dd31a7cc8184371e
Alexcatprg <alexpetrov006@gmail.com>
2024-02-07 20:05:55 +0700

    Введено изменение в файл 2

--- РешуЕГЭ/14688_2.py
+++ РешуЕГЭ/14688_2.py
@@ -1,6 +1,12 @@
-print('x y z')
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            if not(x or y) <= (z == x):
-                print(x, y, z)
+'''
+Сколько существует семеричных шестизначных чисел, содержащих в своей записи ровно одну цифру 3, в которых при этом
+никакие две нечётные цифры не стоят рядом
+'''
+from itertools import product
+col = product('0123456', repeat = 6)
+count = 0
+for w in col:
+    numb = ''.join(w)
+    if '3' in numb and all(int(numb[i]) % 2 != int(numb[i + 1]) % 2 for i in range(len(numb) - 1)):
+        count += 1
+print(count)
